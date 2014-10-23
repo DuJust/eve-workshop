223e38bbd127dd2980b28d751582e3f204ba4828
DuJust <du.just.it@gmail.com>
2014-10-24 00:46:44 +1100

    Add express as a server.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,30 @@
 'use strict';
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+  browserify = require('gulp-browserify');
 
-gulp.task('views', function () {
-  gulp.src('./app/index.html')
-    .pipe(gulp.dest('./dist/index.html'));
+
+var express = require('express'),
+  serverport = 3000;
+
+var server = express();
+server.use(express.static('dist'));
+server.all('/*', function (request, response) {
+  response.sendFile('index.html', {root: 'dist'});
 });
 
+gulp.task('views', function () {
+  gulp.src('app/index.html')
+    .pipe(gulp.dest('dist/'));
+
+  gulp.src('app/views/**/*')
+    .pipe(gulp.dest('dist/views/'));
+});
+
+
 gulp.task('watch', function () {
-  gulp.watch('./app/**/*.html', ['views']);
+  gulp.watch('app/**/*.html', ['views']);
+  server.listen(serverport);
 });
 
 
